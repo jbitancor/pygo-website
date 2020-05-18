50136e0218fbd3a26cd0bf5613d397ebe1bdc751
John <jbitancor@pygo.io>
2020-05-18 14:54:01 -0600

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react";
+import { Link } from "gatsby";
 
-import headerStyles from "../styles/Header.module.scss"
-import logo from "../images/pygo-icon.webp"
-import links from "../constants/links"
+import headerStyles from "../styles/Header.module.scss";
+import logo from "../images/pygo-icon.webp";
+import links from "../constants/links";
 
 const Header = () => {
-  const [isOpen, setNav] = useState(false)
+  const [isOpen, setNav] = useState(false);
   const toggleNav = () => {
-    setNav(isOpen => !isOpen)
-  }
+    setNav((isOpen) => !isOpen);
+  };
 
   return (
     <nav className={headerStyles.navbar}>
       <div className={headerStyles.navCenter}>
         <div className={headerStyles.navHeader}>
-          <Link to="/">
-            <img src={logo} alt="PYGO Inc. Logo" />
+          <Link to='/'>
+            <img src={logo} alt='PYGO Inc. Logo' />
           </Link>
           {/* <button
             type="button"
-            className={headerStyles.logoBtn}
+            className={headerStyles.logoBtn}`
             onClick={toggleNav}
           >
             <FaAlignRight className={headerStyles.logoIcon} />
@@ -31,19 +31,18 @@ const Header = () => {
             isOpen
               ? `${headerStyles.navLinks} ${headerStyles.showNav}`
               : `${headerStyles.navLinks} ${headerStyles.hideNav}`
-          }
-        >
+          }>
           {links.map((item, index) => {
             return (
               <li key={index}>
                 <Link to={item.path}>{item.text}</Link>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
