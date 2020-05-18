42b72a9ded5ff4854a56d38089f6c068daf5a83a
John <jbitancor@pygo.io>
2020-05-18 14:54:59 -0600

    gatsby image

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,72 @@
-import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
+// gatsby image
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 // styles
-import "../styles/globals.scss"
-import aboutStyles from "../styles/About.module.scss"
+import "../styles/globals.scss";
+import aboutStyles from "../styles/About.module.scss";
 // components
-import Introduction from "../components/Introduction"
-import Container from "../components/Container"
+import Introduction from "../components/Introduction";
+import Container from "../components/Container";
 // images
-import team1 from "../images/team-1.jpg"
-import team2 from "../images/team-2.jpg"
-import team3 from "../images/team-3.jpg"
+// import team1 from "../images/team-1.jpg";
+// import team2 from "../images/team-2.jpg";
+// import team3 from "../images/team-3.jpg";
+
+const getImages = graphql`
+  {
+    team1: file(relativePath: { eq: "team-1.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    team2: file(relativePath: { eq: "team-2.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    team3: file(relativePath: { eq: "team-3.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 const AboutPage = () => {
+  const imageData = useStaticQuery(getImages);
+  console.log(imageData);
   return (
     <div>
       <Layout>
         <div className={aboutStyles.bgAbout}>
           <Introduction
-            title="About Us"
-            subtitle="Meet the team that enables connectivity, intelligence, and machine learning
-              capabilities to field devices."
+            title='About Us'
+            subtitle='Meet the team that enables connectivity, intelligence, and machine learning
+              capabilities to field devices.'
           />
         </div>
 
         <div className={aboutStyles.meetTheTeam}>
-          <img src={team1} alt="" />
-          <img src={team2} alt="" />
-          <img src={team3} alt="" />
+          <div className={aboutStyles.imageContainer}>
+            <Img fluid={imageData.team1.childImageSharp.fluid}></Img>
+          </div>
+          <div className={aboutStyles.imageContainer}>
+            <Img fluid={imageData.team2.childImageSharp.fluid}></Img>
+          </div>
+          <div className={aboutStyles.imageContainer}>
+            <Img fluid={imageData.team3.childImageSharp.fluid}></Img>
+          </div>
+          {/* <img src={team1} alt="" /> */}
+          {/* <img src={team2} alt="" /> */}
+          {/* <img src={team3} alt="" /> */}
         </div>
         <Container>
           <p className={aboutStyles.teamSummary}>
@@ -44,7 +83,7 @@ const AboutPage = () => {
         </Container>
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -1,35 +1,35 @@
-import React from "react"
-import Layout from "../components/Layout"
-import "../styles/globals.scss"
-import csStyles from "../styles/CaseStudies.module.scss"
+import React from "react";
+import Layout from "../components/Layout";
+import "../styles/globals.scss";
+import csStyles from "../styles/CaseStudies.module.scss";
 // components
-import CaseStudy from "../components/CaseStudy"
-import Introduction from "../components/Introduction"
+import CaseStudy from "../components/CaseStudy";
+import Introduction from "../components/Introduction";
 // images
-import preview1 from "../images/case-study1.jpg"
+import preview1 from "../images/case-study1.jpg";
 
 const IndustriesPage = () => {
   return (
     <div>
       <Layout>
         <div className={csStyles.bgCaseStudies}>
-          <Introduction title="Case Studies" color="white" />
+          <Introduction title='Case Studies' color='white' />
         </div>
         <CaseStudy
-          title="Centimeter-Level 3D Positioning Using GNSS RTK"
-          subtitle="Applications in monitoring Mine Tailings Ponds, Dam Safety"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, consequuntur. Voluptates ut, incidunt, accusantium adipisci sed quod harum sint ab obcaecati illo quidem explicabo autem quae molestiae officiis neque excepturi recusandae dolor molestias beatae magnam eum officia doloribus porro. Dolorum consequuntur doloribus veniam ex hic autem accusantium ratione ea totam."
+          title='Centimeter-Level 3D Positioning Using GNSS RTK'
+          subtitle='Applications in monitoring Mine Tailings Ponds, Dam Safety'
+          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, consequuntur. Voluptates ut, incidunt, accusantium adipisci sed quod harum sint ab obcaecati illo quidem explicabo autem quae molestiae officiis neque excepturi recusandae dolor molestias beatae magnam eum officia doloribus porro. Dolorum consequuntur doloribus veniam ex hic autem accusantium ratione ea totam.'
           image={preview1}
         />
         <CaseStudy
-          title="Sample Title"
-          subtitle="Sample Subtitle"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, consequuntur. Voluptates ut, incidunt, accusantium adipisci sed quod harum sint ab obcaecati illo quidem explicabo autem quae molestiae officiis neque excepturi recusandae dolor molestias beatae magnam eum officia doloribus porro. Dolorum consequuntur doloribus veniam ex hic autem accusantium ratione ea totam."
+          title='Sample Title'
+          subtitle='Sample Subtitle'
+          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, consequuntur. Voluptates ut, incidunt, accusantium adipisci sed quod harum sint ab obcaecati illo quidem explicabo autem quae molestiae officiis neque excepturi recusandae dolor molestias beatae magnam eum officia doloribus porro. Dolorum consequuntur doloribus veniam ex hic autem accusantium ratione ea totam.'
           image={preview1}
         />
       </Layout>
     </div>
-  )
-}
+  );
+};
 
-export default IndustriesPage
+export default IndustriesPage;
