0bf0e629ec8b276493d43ab429fbaefa417b074d
Katie Lin <chiachen@ualberta.ca>
2020-05-19 10:00:38 -0600

    oops

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,6 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-burger.addEventListener('click', () => {
+burger.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
